1922e348e0358d59b537393df43add275f68b492
Stajyer <Stajyer@DESKTOP-8JV78IH>
2026-08-07 14:05:12 +0300

    PersistenceServiceRegistration yapıldı ve Context classı yazıldı

---
+++ HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
@@ -0,0 +1,49 @@
+﻿using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Persistence.DatabaseContext
+{
+    public class HrDatabaseContext : DbContext
+    {
+        public HrDatabaseContext(DbContextOptions<HrDatabaseContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HrDatabaseContext).Assembly);
+            //IEntityTypeConfiguration<T> arayüzünü uygulayan bütün konfigürasyon sınıflarını otomatik olarak bulur ve DbContext'e uygular.
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker.Entries<BaseEntity>();
+
+            foreach(var entry in base.ChangeTracker.Entries<BaseEntity>()
+                .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))              
+            {
+                entry.Entity.DateModified = DateTime.Now;
+
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<HrDatabaseContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString"));
+            }
+            );
+            return services;
+        }
+    }
+}
