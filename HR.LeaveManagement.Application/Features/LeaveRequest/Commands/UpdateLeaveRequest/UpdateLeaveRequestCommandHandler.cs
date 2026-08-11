f022e4ceb8d762bef6a4d3e53dd50e7d314e4337
Stajyer <Stajyer@DESKTOP-8JV78IH>
2026-08-11 17:49:32 +0300

    UpdateLeaveRequestCommandHandler metodunun içerisi dolduruldu

--- HR.LeaveManagement.Application/Features/LeaveRequest/Commands/UpdateLeaveRequest/UpdateLeaveRequestCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Commands/UpdateLeaveRequest/UpdateLeaveRequestCommandHandler.cs
@@ -1,11 +1,50 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Excepitons;
+using HR.LeaveManagement.Domain;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.UpdateLeaveRequest
 {
-    public class UpdateLeaveRequestCommandHandler : IRequestHandler<>
+    public class UpdateLeaveRequestCommandHandler : IRequestHandler<UpdateLeaveRequestCommand, Unit>
     {
+        private readonly IMapper _mapper;
+        private readonly IEmailSender _emailSender;
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly ILeaveTypeRepository  _leaveTypeRepository;
+        public UpdateLeaveRequestCommandHandler(IMapper mapper, IEmailSender emailSender,
+            ILeaveRequestRepository leaveRequestRepository, ILeaveTypeRepository leaveTypeRepository)
+        {
+            _mapper = mapper;
+            _emailSender = emailSender;
+            _leaveRequestRepository = leaveRequestRepository;
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+        public async Task<Unit> Handle(UpdateLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await _leaveRequestRepository.GetByIdAsync(request.Id);
+            if(leaveRequest == null)
+            {
+                throw new NotFoundException(nameof(LeaveRequest), request.Id);
+            }
+
+            var validator = new UpdateLeaveRequestCommandValidator(_leaveRequestRepository, _leaveTypeRepository);
+
+            var validationResult = await validator.ValidateAsync(request);
+            if (validationResult.Errors.Any())
+            {
+                throw new BadRequestException("Invalid Leave Request", validationResult);
+            }
+
+            _mapper.Map(request, leaveRequest);
+            await _leaveRequestRepository.UpdateAsync(leaveRequest);
+
+            //Send Confirmation email
+            var email = 
+        }
     }
 }
